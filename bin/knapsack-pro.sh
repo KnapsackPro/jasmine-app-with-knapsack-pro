7d6105f1f642063543597e6e95370782a83df228
Rafał Trzop <rafaltrzop@gmail.com>
2018-02-04 17:52:41 +0100

    Remove unnecessary script

--- bin/knapsack-pro.sh
+++ bin/knapsack-pro.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-KNAPSACK_PRO_JS_NPM_DIR="node_modules/knapsack-pro-js"
-
-# remove knapsack-pro-js directory from node_modules
-if [ -d "$KNAPSACK_PRO_JS_NPM_DIR" ]; then
-  rm -rf $KNAPSACK_PRO_JS_NPM_DIR
-fi
-
-# reinstall dependencies (get a fresh copy of knapsack-pro-js)
-npm install
-
 # run knapsack-pro-js
 node_modules/knapsack-pro-js/bin/knapsack-pro.js
