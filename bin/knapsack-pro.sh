ef68c128a318b46cd2e616c477190a9713824062
Rafał Trzop <rafaltrzop@gmail.com>
2018-02-04 17:02:41 +0100

    Add comments to knapsack-pro.sh script

--- bin/knapsack-pro.sh
+++ bin/knapsack-pro.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-NODE_MODULES_KNAPSACK_PRO_JS="node_modules/knapsack-pro-js"
+KNAPSACK_PRO_JS_NPM_DIR="node_modules/knapsack-pro-js"
 
-if [ -d "$NODE_MODULES_KNAPSACK_PRO_JS" ]; then
-  rm -rf $NODE_MODULES_KNAPSACK_PRO_JS
+# remove knapsack-pro-js directory from node_modules
+if [ -d "$KNAPSACK_PRO_JS_NPM_DIR" ]; then
+  rm -rf $KNAPSACK_PRO_JS_NPM_DIR
 fi
 
+# reinstall dependencies (get a fresh copy of knapsack-pro-js)
 npm install
 
+# run knapsack-pro-js
 node_modules/knapsack-pro-js/bin/knapsack-pro.js
